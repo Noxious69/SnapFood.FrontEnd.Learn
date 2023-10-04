e08906b93779e59f233d725ecdba62e327ed6df5
staggteam1920@gmail.com <staggteam1920@gmail.com>
2023-10-04 10:54:01 +0330

    fixed packages 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import { FormsModule , ReactiveFormsModule} from '@angular/forms';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent,
-  ],
+  declarations: [AppComponent, LoginComponent, SignupComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,11 +19,11 @@ import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
     NgbModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    NgModule,
   ],
-  
+
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
